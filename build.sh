2ffb2f79de3727575e14b08b1436d9872ce2ea54
alanalava <alanwladimir.alavaalava@gmail.com>
2025-12-31 00:45:03 -0500

    correcion de build

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ python manage.py migrate
 # Crear/Asegurar superusuario (Esto sí se deja siempre)
 echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
-User.objects.filter(username='Alan').exists() or \
-User.objects.create_superuser('Alan', 'Alan@gmail.com', '123456789')" \
+User.objects.filter(username='Jandry').exists() or \
+User.objects.create_superuser('Jandry', 'Jandry@gmail.com', 'NCQM200406')" \
 | python manage.py shell
